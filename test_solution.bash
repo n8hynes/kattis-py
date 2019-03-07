56b5a294941dd2b1f9211ae630303a12b9a348c2
n8hynes <n8hynes@gmail.com>
2019-03-07 13:37:41 -0400

    Check solution against sample files

--- test_solution.bash
+++ test_solution.bash
@@ -48,7 +48,42 @@ function main {
 		die "Missing required argument: <problem_name>\n${__USAGE__}"
 	fi
 
-	
+	local directory="problems/${problem}"
+
+	if [[ -d "${directory}" ]]; then
+		cd "${directory}" || die "Cannot navigate to directory"
+		if [[ -d "samples/" ]]; then
+			local x=1
+			for i in samples/*.in; do
+				touch "output"
+				touch "error"
+				python "${problem}.py" < "$i" > "output" 2> "error"
+				exit_status="$?"
+				if [[ ${exit_status} != 0 ]]; then
+					echo -e "\nTEST ${x}: RUNTIME ERROR"
+					echo "STDERR:"
+					cat "error"
+				elif ! cmp -s "output" "${i:0:-3}.ans"; then
+					echo -e "\nTEST ${x}: FAIL"
+					echo "STDERR:"
+					cat "error"
+					echo "STDOUT:"
+					cat "output"
+					echo "EXPECTED:"
+					cat "${i:0:-3}.ans"
+				else
+					echo -e "\nTEST ${x}: PASS"
+				fi
+				rm "output"
+				rm "error"
+				let x+=1
+			done
+		else
+			echo "There are no sample files to test!"
+		fi
+	else
+		echo "No such problem to test."
+	fi
 
 }
 
