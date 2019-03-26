800b73923fe98108e31f834c9520d768ab14e277
n8hynes <n8hynes@gmail.com>
2019-03-26 18:39:59 -0300

    Change solution to 'Phone List'
    Solved timeout issue by changing solution method

--- problems/phonelist/phonelist.py
+++ problems/phonelist/phonelist.py
@@ -1,48 +1,29 @@
 import sys
-import itertools
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
-def consistency_check(num_1, num_2):
-    """ 
-    Checks if 2 phone numbers are inconsistent.
-    Requires len(num_1) <= len(num_2)
-    """
-    if (num_2[:len(num_1)] == num_1): return False
-    else: return True
-
-def check_num_list(nums):
-    """
-    Checks each pair of items in the list for consistency.
-    """
-    consistent = True
-    nums.sort(key = len)
-    for x in range(len(nums)):
-        for y in range(x + 1, len(nums)):
-            if not consistency_check(nums[x], nums[y]):
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        nums = []
+        for _ in range(n):
+            nums.append(input())
+        nums = sorted(nums, key=len, reverse=True)
+        d = set()
+        consistent = True
+        for num in nums:
+            if not consistent: break
+            if num in d:
                 consistent = False
                 break
-        if not consistent: break
-    return consistent
+            for i in range(1, len(num)+1):
+                d.add(num[:i])
 
-
-def main():
-    t = int(sys.stdin.readline())
-    for _ in range(t):
-        n = int(sys.stdin.readline())
-        num_list = [[] for x in range(10)]
-        consistent = True
-        for _ in range(n):
-            new_num = sys.stdin.readline().rstrip()
-            num_list[int(new_num[0])].append(new_num)
-        for bucket in num_list:
-            consistent = check_num_list(bucket)
-            if not consistent: break
         if consistent: print("YES")
         else: print("NO")
-            
-            
+ 
 
 main()
 
